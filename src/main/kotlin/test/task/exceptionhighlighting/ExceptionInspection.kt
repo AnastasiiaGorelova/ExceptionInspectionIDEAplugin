eb383d7f8476726736810d1805321e296ab54495
Anastasiia Gorelova <agorelova@Anastasiias-MacBook-Air.local>
2022-10-30 01:03:10 +0200

    in process

--- src/main/kotlin/test/task/exceptionhighlighting/ExceptionInspection.kt
+++ src/main/kotlin/test/task/exceptionhighlighting/ExceptionInspection.kt
@@ -3,6 +3,7 @@ package test.task.exceptionhighlighting
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
+import org.jetbrains.kotlin.idea.util.isAnonymousFunction
 import org.jetbrains.kotlin.psi.*
 
 
@@ -12,116 +13,78 @@ class ExceptionInspection : AbstractKotlinInspection() {
             holder: ProblemsHolder,
             isOnTheFly: Boolean
     ): KtVisitorVoid {
-        return classVisitor { ktClass ->
-            if (ktClass.isData()) {
-                // check if serialized name annotation is required.
-                val param = ktClass.isSerializedNameAnnotationMissing()
-                param?.let {
-                    holder.registerProblem(
-                            it as PsiElement,
-                            "Missing SerializedName annotation"
-                    )
-                }
+//        return buildVisitor(holder, isOnTheFly).visitThrowExpression()
+        return namedFunctionVisitor { ktNamedFunction ->
+            val param = ktNamedFunction.isExceptionThrown()
+            param?.let {
+                holder.registerProblem(
+                        it as PsiElement,
+                        "Exception is thrown here!"
+                )
             }
         }
     }
+
+//    override fun buildVisitor(
+//            holder: ProblemsHolder,
+//            isOnTheFly: Boolean
+//    ): KtVisitorVoid {
+//        return classVisitor { ktClass ->
+//            if (ktClass.isData()) {
+//                // check if serialized name annotation is required.
+//                val param = ktClass.isSerializedNameAnnotationMissing()
+//                param?.let {
+//                    holder.registerProblem(
+//                            it as PsiElement,
+//                            "Exception is thrown here!"
+//                    )
+//                }
+//            }
+//        }
+//    }
+
 }
 
-fun KtClass.isSerializedNameAnnotationMissing(): KtParameter? {
-    this.getPrimaryConstructorParameterList()?.parameters?.forEach { param ->
-        if (!param.annotationEntries.any {
-                    it.shortName?.asString() == "SerializedName"
-                }
-        ) {
-            return param
+fun KtNamedFunction.isExceptionThrown(): KtThrowExpression? {
+    val bodyExpression = this.bodyExpression
+    if (bodyExpression is KtThrowExpression)
+        return bodyExpression
+
+
+//    bodyExpression?.children?.forEach { expression ->  //TODO check for anonymous function
+//        if (expression is KtThrowExpression)
+//            return expression
+//    }
+//
+//    return null
+
+
+    return helper(bodyExpression)
+}
+
+private fun helper(element: PsiElement?): KtThrowExpression? {
+    element?.children?.forEach { expression ->  //TODO check for anonymous function
+        if (expression is KtThrowExpression)
+            return expression
+        else {
+            val res = helper(expression)
+            if(res != null) {
+                return res
+            }
         }
     }
     return null
 }
 
-
-
-//class ExceptionInspection: AbstractKotlinInspection() {
-//    override fun getDisplayName(): String {
-//        return "Use CamelCase naming"
-//    }
-//
-////    override fun getGroupDisplayName(): String { //TODO
-////        return GroupNames.STYLE_GROUP_NAME
-////    }
-//
-//    override fun getShortName(): String {
-//        return "Camelcase"
-//    }
-//
-//    override fun buildVisitor(holder: ProblemsHolder,
-//                              isOnTheFly: Boolean): KtVisitorVoid {
-//        return namedDeclarationVisitor { declaredName ->
-//            if (declaredName.name?.isDefinedCamelCase() == false) {
-//                println(
-//                        "Non CamelCase Name Detected for ${declaredName.name}")
-//                holder.registerProblem(
-//                        declaredName.nameIdentifier as PsiElement,
-//                        "Please use CamelCase for #ref #loc")
-//            }
+//fun KtClass.isSerializedNameAnnotationMissing(): KtParameter? {
+//    this.getPrimaryConstructorParameterList()?.parameters?.forEach { param ->
+//        if (!param.annotationEntries.any {
+//                    it.shortName?.asString() == "SerializedName"
+//                }
+//        ) {
+//            return param
 //        }
 //    }
-//
-//    private fun String.isDefinedCamelCase(): Boolean {
-//        val toCharArray = toCharArray()
-//        return toCharArray
-//                .mapIndexed { index, current ->
-//                    current to toCharArray.getOrNull(index + 1) }
-//                .none { it.first.isUpperCase() &&
-//                        it.second?.isUpperCase() ?: false }
-//    }
-//
-//    override fun isEnabledByDefault(): Boolean {
-//        return true
-//    }
-//
+//    return null
 //}
 
-/*
-class CamelcaseInspection : AbstractKotlinInspection() {
-
-    override fun getDisplayName(): String {
-        return "Use CamelCase naming"
-    }
-
-    override fun getGroupDisplayName(): String {
-        return GroupNames.STYLE_GROUP_NAME
-    }
-
-    override fun getShortName(): String {
-        return "Camelcase"
-    }
-
-    override fun buildVisitor(holder: ProblemsHolder,
-                              isOnTheFly: Boolean): KtVisitorVoid {
-        return namedDeclarationVisitor { declaredName ->
-            if (declaredName.name?.isDefinedCamelCase() == false) {
-                System.out.println(
-                        "Non CamelCase Name Detected for
-                        ${declaredName.name}")
-                holder.registerProblem(
-                        declaredName.nameIdentifier as PsiElement,
-                        "Please use CamelCase for #ref #loc")
-            }
-        }
-    }
-
-    private fun String.isDefinedCamelCase(): Boolean {
-        val toCharArray = toCharArray()
-        return toCharArray
-                .mapIndexed { index, current ->
-                    current to toCharArray.getOrNull(index + 1) }
-                .none { it.first.isUpperCase() &&
-                        it.second?.isUpperCase() ?: false }
-    }
-
-    override fun isEnabledByDefault(): Boolean {
-        return true
-    }
-}
-*/
