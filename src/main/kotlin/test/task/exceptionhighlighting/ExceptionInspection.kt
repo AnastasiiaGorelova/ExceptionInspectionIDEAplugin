fe1fa02f90ad8b22974493b28edc8a7c63977c52
Anastasiia Gorelova <agorelova@Anastasiias-MacBook-Air.local>
2022-10-30 14:00:01 +0100

    refactored

--- src/main/kotlin/test/task/exceptionhighlighting/ExceptionInspection.kt
+++ src/main/kotlin/test/task/exceptionhighlighting/ExceptionInspection.kt
@@ -3,7 +3,6 @@ package test.task.exceptionhighlighting
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
-import org.jetbrains.kotlin.idea.util.isAnonymousFunction
 import org.jetbrains.kotlin.psi.*
 
 
@@ -13,78 +12,30 @@ class ExceptionInspection : AbstractKotlinInspection() {
             holder: ProblemsHolder,
             isOnTheFly: Boolean
     ): KtVisitorVoid {
-//        return buildVisitor(holder, isOnTheFly).visitThrowExpression()
         return namedFunctionVisitor { ktNamedFunction ->
-            val param = ktNamedFunction.isExceptionThrown()
-            param?.let {
+            val throwExpression = ktNamedFunction.analyzeExceptions()
+            throwExpression?.let {
                 holder.registerProblem(
-                        it as PsiElement,
+                        it,
                         "Exception is thrown here!"
                 )
             }
         }
     }
 
-//    override fun buildVisitor(
-//            holder: ProblemsHolder,
-//            isOnTheFly: Boolean
-//    ): KtVisitorVoid {
-//        return classVisitor { ktClass ->
-//            if (ktClass.isData()) {
-//                // check if serialized name annotation is required.
-//                val param = ktClass.isSerializedNameAnnotationMissing()
-//                param?.let {
-//                    holder.registerProblem(
-//                            it as PsiElement,
-//                            "Exception is thrown here!"
-//                    )
-//                }
-//            }
-//        }
-//    }
-
-}
-
-fun KtNamedFunction.isExceptionThrown(): KtThrowExpression? {
-    val bodyExpression = this.bodyExpression
-    if (bodyExpression is KtThrowExpression)
-        return bodyExpression
-
-
-//    bodyExpression?.children?.forEach { expression ->  //TODO check for anonymous function
-//        if (expression is KtThrowExpression)
-//            return expression
-//    }
-//
-//    return null
-
-
-    return helper(bodyExpression)
-}
-
-private fun helper(element: PsiElement?): KtThrowExpression? {
-    element?.children?.forEach { expression ->  //TODO check for anonymous function
-        if (expression is KtThrowExpression)
-            return expression
-        else {
-            val res = helper(expression)
-            if(res != null) {
-                return res
-            }
-        }
+    private fun KtNamedFunction.analyzeExceptions(): KtThrowExpression? {
+        val bodyExpression = this.bodyExpression
+        return findThrowExpression(bodyExpression)
     }
-    return null
+
+    private fun findThrowExpression(element: PsiElement?): KtThrowExpression? {
+        if (element is KtThrowExpression)
+            return element
+        element?.children?.forEach { e ->
+            val foundThrowExpression = findThrowExpression(e)
+            foundThrowExpression?.let { return it }
+        }
+        return null
+    }
+
 }
-
-//fun KtClass.isSerializedNameAnnotationMissing(): KtParameter? {
-//    this.getPrimaryConstructorParameterList()?.parameters?.forEach { param ->
-//        if (!param.annotationEntries.any {
-//                    it.shortName?.asString() == "SerializedName"
-//                }
-//        ) {
-//            return param
-//        }
-//    }
-//    return null
-//}
-
